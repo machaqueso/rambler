26a7dad3dd76c4e068b476cab32aea03ad31b0c5
machaqueso <machaqueso@gmail.com>
2016-08-10 18:18:58 -0500

    Set expiration token

--- Models/GoogleToken.cs
+++ Models/GoogleToken.cs
@@ -5,9 +5,19 @@ namespace Rambler
 
     public class GoogleToken
     {
+        private int _expiresIn;
+
         public string access_token { get; set; }
         public string token_type { get; set; }
-        public int expires_in { get; set; }
+        public int expires_in
+        {
+            get { return _expiresIn; }
+            set
+            {
+                _expiresIn = value;
+                this.ExpirationDate = DateTime.Now.AddSeconds(_expiresIn);
+            }
+        }
         public string refresh_token { get; set; }
         public DateTime ExpirationDate { get; set; }
     }
