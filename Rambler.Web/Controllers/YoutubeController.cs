7f2676884de24790a651d51ef020d1f62ca97df1
machaqueso@machaqueso.cl <machaqueso@machaqueso.cl>
2019-01-05 17:19:41 -0600

    Created base youtube integration page

---
+++ Rambler.Web/Controllers/YoutubeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rambler.Web.Controllers
+{
+    public class YoutubeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
