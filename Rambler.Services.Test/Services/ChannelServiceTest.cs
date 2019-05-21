fda49a41b52e0fb63b68450d87eab16d9f5e943e
machaqueso@machaqueso.cl <machaqueso@machaqueso.cl>
2019-05-20 19:46:56 -0500

    Completed coverage for ChannelService

--- Rambler.Services.Test/Services/ChannelServiceTest.cs
+++ Rambler.Services.Test/Services/ChannelServiceTest.cs
@@ -6,6 +6,8 @@ using Rambler.Models;
 using Rambler.Web.Services;
 using System;
 using System.Linq;
+using System.Threading.Tasks;
+using AutoFixture;
 using Xunit;
 
 namespace Rambler.Services.Test.Services
@@ -15,6 +17,7 @@ namespace Rambler.Services.Test.Services
         private readonly IConfiguration configuration;
         private readonly DataContext dataContext;
         private readonly ChannelService channelService;
+        private IFixture fixture;
 
         public ChannelServiceTest()
         {
@@ -32,6 +35,8 @@ namespace Rambler.Services.Test.Services
             dataContext.Database.EnsureDeleted();
 
             channelService = new ChannelService(dataContext);
+
+            fixture = new Fixture();
         }
 
         [Fact]
@@ -39,6 +44,35 @@ namespace Rambler.Services.Test.Services
         {
             var channels = channelService.GetChannels();
             channels.Should().BeAssignableTo<IQueryable<Channel>>();
+        }
+
+        [Fact]
+        public async Task Given_existing_channel_UpdateChannel_updates_it()
+        {
+            var channel = fixture.Build<Channel>()
+                .Without(x => x.Id)
+                .Create();
+
+            await dataContext.AddAsync(channel);
+            await dataContext.SaveChangesAsync();
+
+            var channelToUpdate = await channelService.GetChannels().SingleOrDefaultAsync(x => x.Id == channel.Id);
+            channelToUpdate.Name = fixture.Create<string>();
+
+            await channelService.UpdateChannel(channel.Id, channelToUpdate);
+
+            Assert.True(channelService.GetChannels().Any(x => x.Id == channel.Id
+                                                              && x.Name == channelToUpdate.Name));
+        }
+
+        [Fact]
+        public async Task Given_nonexistent_channel_UpdateChannel_throws()
+        {
+            var id = fixture.Create<int>();
+            var channel = fixture.Build<Channel>()
+                .Create();
+
+            await Assert.ThrowsAsync<InvalidOperationException>(() => channelService.UpdateChannel(id, channel));
         }
 
     }
