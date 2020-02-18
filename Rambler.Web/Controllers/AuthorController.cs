ad5b93256040fcaadc410705edb52716a9aca0ed
machaqueso <machaqueso@machaqueso.cl>
2020-02-17 19:44:53 -0800

    Added author details page

--- Rambler.Web/Controllers/AuthorController.cs
+++ Rambler.Web/Controllers/AuthorController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Rambler.Services;
 
 namespace Rambler.Web.Controllers
 {
     public class AuthorController : Controller
     {
+        private readonly AuthorService authorService;
+
+        public AuthorController(AuthorService authorService)
+        {
+            this.authorService = authorService;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -12,6 +22,20 @@ namespace Rambler.Web.Controllers
         public IActionResult Filter()
         {
             return View();
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            var author = await authorService.GetAuthors()
+                .Include(x => x.ChatMessages)
+                .Include(x => x.AuthorFilters)
+                .Include(x => x.AuthorScoreHistories)
+                .SingleOrDefaultAsync(x => x.Id == id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            return View(author);
         }
     }
 }
