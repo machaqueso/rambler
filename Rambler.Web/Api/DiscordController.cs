d9afb636bce51191f26fe015c2e8fde30c73c5eb
machaqueso <machaqueso@machaqueso.cl>
2020-03-19 22:10:52 -0500

    Moved api configuration pages inside configuration, added discord details

--- Rambler.Web/Api/DiscordController.cs
+++ Rambler.Web/Api/DiscordController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Rambler.Services;
@@ -18,7 +19,7 @@ namespace Rambler.Web.Api
         }
 
         [Route("status")]
-        public async Task< IActionResult> GetStatus()
+        public async Task<IActionResult> GetStatus()
         {
             var status = await discordService.GetStatus();
 
@@ -26,37 +27,60 @@ namespace Rambler.Web.Api
         }
 
         [Route("guild")]
-        public async Task< IActionResult> GetGuilds()
+        public async Task<IActionResult> GetGuilds()
         {
             var guilds = await discordService.GetGuilds();
 
-            return Ok(guilds.Select(x=>new
+            return Ok(guilds.Select(x => new
             {
-                x.Id,
+                Id = x.Id.ToString(),
                 x.Name,
                 x.MemberCount
             }));
         }
 
         [Route("channel")]
-        public async Task< IActionResult> GetChannels()
+        public async Task<IActionResult> GetChannels()
         {
             var guilds = await discordService.GetGuilds();
             var channels = guilds.SelectMany(x => x.Channels);
 
-            return Ok(channels.Select(x=>new
+            return Ok(channels.Select(x => new
             {
                 x.Id,
                 x.Name
             }));
         }
 
+        [Route("guild/{id}/textChannel")]
+        public async Task<IActionResult> GetTextChannels(ulong id)
+        {
+            var guild = (await discordService.GetGuilds()).FirstOrDefault(x => x.Id == id);
+
+            if (guild == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(guild.TextChannels
+                .OrderBy(x => x.Position)
+                .Select(x => new
+                {
+                    x.Id,
+                    x.Name,
+                    x.Position,
+                    x.IsNsfw,
+                    x.Mention,
+                    x.Topic
+                }));
+        }
+
         [Route("defaultChannel")]
-        public async Task< IActionResult> GetDefaultChannel()
+        public async Task<IActionResult> GetDefaultChannel()
         {
             var guilds = await discordService.GetGuilds();
 
-            return Ok(guilds.Select(x=>new
+            return Ok(guilds.Select(x => new
             {
                 x.DefaultChannel.Id,
                 x.DefaultChannel.Name
---
+++ Rambler.Web/Controllers/DiscordController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rambler.Web.Controllers
+{
+    public class DiscordController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Channel(ulong id)
+        {
+            return View(id);
+        }
+
+    }
+}
